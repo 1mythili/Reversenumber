de04dc6b480ea7c0f8cb00f1f84f85882f37a2f8
1mythili <jaimyaa98@gmail.com>
2022-01-13 01:13:26 -0800

    Reverse Number

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reverse_number
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReverseExample.Reversenumber();
+        }
+    }
+}
---
+++ reversenumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_number
+{
+    public class ReverseExample
+    {
+        public static void Reversenumber()
+        {
+            int n, reverse = 0, rem;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
